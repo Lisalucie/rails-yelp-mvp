d8c7d15b4316340b44e1bd3bef26d063f9a5ebbd
lisa loreau <lisalor44@gmail.com>
2022-05-18 17:03:41 +0200

    yelp MVC complet refactoré

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+dichinese = { name: 'Dichinese', address: '7 Boundary St, London E2 7JE', category: 'chinese' }
+pizza_belg =  { name: 'Pizza Belg', address: '56A Shoreditch High St, London E1 6PQ', category: 'belgian' }
+gaijin_food = { name: 'Gaijin Food', address: 'pont saint martin, France', category: 'japanese' }
+betsubara = { name: 'Betsubara', address: 'somewhere in japan, tokyo', category: 'japanese' }
+chez_french = { name: 'Chez French', address: 'dans Nantes, Nantes 44400', category: 'french' }
+
+[dichinese, pizza_belg, gaijin_food, betsubara, chez_french].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
